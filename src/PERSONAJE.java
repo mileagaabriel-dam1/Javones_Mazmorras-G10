c93a7c217de03724ea9b1e3a937b69db2047eb26
ian-yepa <ianrubiosanchez17@alumnes.ilerna.com>
2026-02-07 17:31:37 +0100

    Update PERSONAJE #2

--- src/PERSONAJE.java
+++ src/PERSONAJE.java
@@ -1,9 +1,16 @@
 public class PERSONAJE {
-    // Atributos
     private String nombre;
     private int nivel;
     private int puntosVida;
     private boolean protegido;
     private Inventario inventario;
     
+    public PERSONAJE(String nombre) {
+        this.nombre = nombre;
+        this.nivel = 1;
+        this.puntosVida = 100;
+        this.protegido = false;
+        this.inventario = new Inventario();
+    }
+
 }
