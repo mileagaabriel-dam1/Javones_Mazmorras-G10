fa7caea87e860fab4dbb8f341801e747d54463ea
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-02-07 14:25:32 +0100

    Update MAGO.java

--- src/MAGO.java
+++ src/MAGO.java
@@ -11,4 +11,21 @@ public class MAGO extends PERSONAJE {
         
     }
     //Cogemos el "MAGO" de PERSONAJE, el nombre y le asignamos un valor a mana.
- 
+    
+    public MAGO(String nombre, int manaInicial) {
+        super(nombre);
+        this.mana = manaInicial;
+    }
+    
+    public void lanzarHechizo(int costeMana) {
+        if (costeMana <= mana) {
+            mana -= costeMana;
+            System.out.println(getNombre() + " lanza un hechizo que consume " + costeMana + " puntos de maná.");
+            System.out.println("Maná restante: " + mana);
+        } else {
+            System.out.println(getNombre() + " intenta lanzar un hechizo pero no tiene suficiente maná.");
+            System.out.println("Maná necesario: " + costeMana + ", Maná disponible: " + mana);
+        }
+    }
+    
+    //La parte de los hechizos, si hay suficiente mana, se gastara, sino la hay se mostrara por pantalla la que se necesita.
